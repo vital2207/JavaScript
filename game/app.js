dbe9c62be273eb2b2a1327c49cbea67f31fefa8b
vital2207 <vital2207@yandex.ru>
2017-05-18 00:12:45 +0300

    game commit

--- game/app.js
+++ game/app.js
@@ -10,14 +10,64 @@ GAME RULES:
 */
 
 var scores = [0,0];
-var roundScore1 = 0;
+var roundScore = 0;
 
 var activePlayer = 0;
 
-var dice = Math.floor(Math.random()*6)+1;
 
-document.querySelector('#score-' + activePlayer).textContent = dice;
+//
 //document.querySekector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-var x = document.querySelector('#score-0').textContent;
+//var x = document.querySelector('#score-0').textContent;
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+   var dice = Math.floor(Math.random()*6)+1;
+
+   var diceDom = document.querySelector('.dice');
+   diceDom.style.display = 'block';
+   diceDom.src = 'dice-'+ dice + '.png';
+
+
+   if(dice !== 1){
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+   }else{
+        nextPlayer();
+   }
+
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+        scores[activePlayer] += roundScore;
+
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        if(scores[activePlayer] >=100){
+            document.querySelector('#name-'+activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.dispaly = 'none';
+            document.querySelector('.player-'+activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-'+activePlayer + '-panel').classList.remove('active');
+        } else{
+             nextPlayer();
+        }
+       
+});
+
+function nextPlayer(){
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+       // document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+        document.querySelector('.dice').style.display = 'none';
+}
