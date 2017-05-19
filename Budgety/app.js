c930673b9c358c1b1a8cd14cf43fecf995f93786
vital2207 <vital2207@yandex.ru>
2017-05-19 22:56:19 +0300

    create events

--- Budgety/app.js
+++ Budgety/app.js
@@ -1,19 +1,10 @@
+//BUDGET CONTROLLER
 var budgetController = (function(){
 
-    var x = 23;
-
-    var add = function(a){
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b){
-            return add(b);
-        } 
-    }
+   
 })();
 
-
+//UI CONTROLLER
 var UIController = (function(){
 
     
@@ -22,14 +13,32 @@ var UIController = (function(){
 })();
 
 
+//GLOBAL APP CONTROLLER
+
 var controller = (function(budgetCtrl, UICtrl){
 
-   var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function(){
+         //1. Get the field input data
 
-   return {
-       anotherPublic: function(){
-           console.log(z);
-       }
-   }
+        //2. Add the item to the budget controller
+
+        //3. Add the item to the UI
+
+        //4.Calculate the budget
+
+        //5.Display the budget on the UI
+        console.log('test');
+
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function(e){
+       
+        if(e.key === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+
+    });
 
 })(budgetController, UIController);
+
