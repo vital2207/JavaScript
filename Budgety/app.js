93b71b62cdfc952b3415c1f248e5168f081a2203
vital2207 <vital2207@yandex.ru>
2017-05-25 23:51:47 +0300

    added UIcontroller public method

--- Budgety/app.js
+++ Budgety/app.js
@@ -7,7 +7,25 @@ var budgetController = (function(){
 //UI CONTROLLER
 var UIController = (function(){
 
-    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    };
+
+    return{
+        getinput: function(){
+            return{
+                type : document.querySelector(DOMstrings.inputType).value,
+                description : document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value
+            }; 
+        },
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
 
     
 })();
@@ -17,9 +35,12 @@ var UIController = (function(){
 
 var controller = (function(budgetCtrl, UICtrl){
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function(){
          //1. Get the field input data
-
+         var input = UICtrl.getinput();
+         console.log(input);
         //2. Add the item to the budget controller
 
         //3. Add the item to the UI
@@ -27,11 +48,10 @@ var controller = (function(budgetCtrl, UICtrl){
         //4.Calculate the budget
 
         //5.Display the budget on the UI
-        console.log('test');
-
+       
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
     document.addEventListener('keypress', function(e){
        
         if(e.key === 13 || event.which === 13){
